97e93c3ad2dcf09c66c301abc550d0b07b86b0aa
Kenneth Li <likenhp@gmail.com>
2019-06-06 16:42:03 -0700

    changed poekmon to the witcher, added dynamic image creation, created basic logic for card flipping, cards have the ablitlity to be matched

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 $(document).ready(intializeApp);
 
-var game;
+
 var gameplay;
 
 
 function intializeApp(){
-    game = new Gameboard();
-    game.renderCard();
     gameplay = new Game();
+    gameplay.startGame();
     gameplay.addEventListener();
 }
